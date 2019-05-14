443f0e55f7860b5b725a1c7c8d8147a761da1929
dimqa327 <50614385+dimqa327@users.noreply.github.com>
2019-05-14 19:15:08 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+
+Bot = commands.Bot(command_prefix="!")
+
+@Bot.command()
+async def hello(ctx):
+   author = ctx.message.author
+   await ctx.send(f"Приветствую! {author.mention}")
+
+@Bot.command()
+@commands.has_permissions(administrator= True)
+async def mute(ctx, member: discord.Member):
+   mute_role = discord.utils.get(ctx.message.guild.roles, name= "mute")
+   await member.add_roles(mute_role)
+Bot.run(open('token.txt','r').readline())
